9715d2dff50e5722a6dd54ce5a20a2d18c5e1b23
un-olk <olk.zaytsev@gmail.com>
2015-10-13 15:34:07 +0300

    Wrote a documentation for Vector

--- src/sparse/vector.h
+++ src/sparse/vector.h
@@ -4,18 +4,39 @@
 #include "vectorbase.h"
 #include "exception.h"
 
+/**
+ * @brief Reimplemented mathematical vector
+ * @details Provides basic arithmetic operators
+ * 
+ * @tparam T Type of data stored in vector 
+ */
 template <typename T>
 class Vector : public VectorBase<T>
 {
 	T *_arr;
 
 public:
+	/**
+	 * @brief Creates an instance of empty Vector
+	 * @details Allocates memory for given number
+	 * of elements. They must be inserted later.
+	 * 
+	 * @param size Number of elements
+	 */
 	Vector(int size)
 		: VectorBase<T>(size)
 	{
 		_arr = new T[size];
 	}
 
+	/**
+	 * @brief Creates an instance of Vector
+	 * @details The elements are coppied from given
+	 * plain array
+	 * 
+	 * @param arr Plain array
+	 * @param size Number of elements
+	 */
 	Vector(T *arr, int size)
 		: VectorBase<T>(size)
 	{
@@ -26,6 +47,12 @@ public:
 		}
 	}
 
+	/**
+	 * @brief Copies the data of Vector from
+	 * other Vector
+	 * 
+	 * @param other Reference to other Vector
+	 */
 	Vector(const Vector &other)
 	{
 		this->_size = other._size;
@@ -36,11 +63,21 @@ public:
 		}
 	}
 
+	/**
+	 * @brief Deletes an instance of Vector
+	 */
 	~Vector()
 	{
 		delete[] _arr;
 	}
 
+	/**
+	 * @brief Assignes an instance of Vector with
+	 * another Vector.
+	 * @details Copies all the data from other Vector
+	 * 
+	 * @param other Reference to other Vector
+	 */
 	Vector& operator= (const Vector &other)
 	{
 		this->_size = other._size;
@@ -53,11 +90,31 @@ public:
 		return *this;
 	}
 
+	/**
+	 * @brief Gets and sets the element by it's position
+	 * in Vector
+	 * @details This operator does not check if the index
+	 * is out of range. Therefore it's less time-expensive
+	 * but unsafe.
+	 * 
+	 * @param index Position in Vector
+	 */
 	T& operator[] (int index)
 	{
 		return _arr[index];
 	}
 
+	//TODO: Improve documentation
+
+	/**
+	 * @brief Gets and sets the element by it's position
+	 * in Vector
+	 * @details This method checks if the index
+	 * is out of range. Therefore it's more time-expensive
+	 * but safe.
+	 * 
+	 * @param index Position in Vector
+	 */
 	T& at(int index)
 	{
 		if (index < 0 || index >= this->_size) {
@@ -67,16 +124,44 @@ public:
 		return _arr[index];
 	}
 
+	//TODO: Improve documentation
+
+	/**
+	 * @brief Gets the element by it's position in Vector
+	 * @details This method does not check if the index
+	 * is out of range. Therefore it's less time-expensive
+	 * but unsafe. Being a const-method it is guaranteed
+	 * not to modify the data inside the vector.
+	 * 
+	 * @param index Position in Vector
+	 */
 	T get(int index) const
 	{
 		return _arr[index];
 	}
 
+	/**
+	 * @brief Inserts an element to Vector
+	 * @details The space for this element should
+	 * already be allocated by Vector(int)
+	 * constructor.
+	 * 
+	 * @param val Value to be inserted
+	 * @param i Position in Vector
+	 */
 	void insert(const T &val, int index)
 	{
 		_arr[index] = val;
 	}
 
+	/**
+	 * @brief Implements the elementwise sume of
+	 * two Vector-s.
+	 * 
+	 * @param other Second vector (it will be added
+	 * to this one)
+	 * @return Sume of two vectors
+	 */
 	Vector operator+ (const Vector &other)
 	{
 		if (this->_size != other._size) {
@@ -92,6 +177,14 @@ public:
 		return res;
 	}
 
+	/**
+	 * @brief Implements the elementwise difference of
+	 * two Vector-s.
+	 * 
+	 * @param other Second vector (it will be substracted
+	 * from this one)
+	 * @return Difference of two vectors
+	 */
 	Vector operator- (const Vector &other)
 	{
 		if (this->_size != other._size) {
@@ -107,6 +200,15 @@ public:
 		return res;
 	}
 
+	/**
+	 * @brief Implements multiplication of vector
+	 * on given scalar value.
+	 * @details Each element of vector is multiplied
+	 * by scalar value.
+	 * 
+	 * @param val Scalar value
+	 * @return Vector-product
+	 */
 	template <typename S>
 	Vector operator* (const S &val) const
 	{
@@ -119,6 +221,15 @@ public:
 		return res;
 	}
 	
+	/**
+	 * @brief Implements division of vector by given
+	 * scalar value.
+	 * @details Each element of vector is divided
+	 * by scalar value.
+	 * 
+	 * @param val Scalar value
+	 * @return Vector-division
+	 */
 	template <typename S>
 	Vector operator/ (const S &val) const
 	{
@@ -136,6 +247,18 @@ public:
 	}
 };
 
+/**
+ * @brief Implements multiplication of scalar value
+ * on Vector.
+ * @details Makes cector-scalar multiplication
+ * commutative.
+ * 
+ * @param val Scalar value
+ * @param vec Vector
+ * @tparam T Type of data stored in Vector
+ * @tparam S Type of scalar value
+ * @return Vector-product
+ */
 template <typename T, typename S>
 Vector<T> operator* (const S &val, const Vector<T> &vec)
 {
